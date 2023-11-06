6a8070ac10515003a57b2ef555270ac7c10c7176
Siddarth <110726331+siddarth2810@users.noreply.github.com>
2023-11-06 23:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const _ = require("lodash");
 const path = require("path");
 
 const app = express();
+app.use(express.static("public"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+
 
 const mongoose = require("mongoose");
 const uri = ""
