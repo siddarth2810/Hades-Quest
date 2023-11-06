bc161b5f483b1483e159ebb55f11c4cf936f656c
Siddarth <110726331+siddarth2810@users.noreply.github.com>
2023-11-07 01:00:57 +0530

    Update app.js
    Fixed the path errors of the public folder

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const path = require("path");
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
